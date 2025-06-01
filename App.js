4ed1aade9737dcbe318ba89b2326e62ae76e924d
AnkitaJain-25 <ankita.jain25@outlook.com>
2025-06-01 20:42:00 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,35 +1,16 @@
-/**
- *
- * <div id="parent">
- *  <div id="child">
- *      <h1>I'm an h1 tag</h1>
- *      <h2>I'm an h1 tag</h2>
- *  </div>
- *  <div id="child2">
- *      <h1>I'm an h1 tag</h1>
- *      <h2>I'm an h1 tag</h2>
- *  </div>
- * </div>
- *
- * ReactElement(Object) ==> HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"), //for sibling create array of children
+    React.createElement("h1", {}, "This is namaste food"),
+    React.createElement("h2", {}, "By ankita jain"), //for sibling create array of children
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag"), //for sibling create array of children
   ]),
 ]);
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # food App
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Fasterb Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consisting Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - Remove unused code
+- Different dev and prod bundles
